0f08aa1f28537e9c6db8565290b227f5ec4fcb77
Brandon Austin <tarwine0406@gmail.com>
2021-02-10 20:28:21 -0800

    Update sample test

---
+++ Assets/Tests/PlayMode/ValidateSampleScene.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class ValidateSampleScene
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void ValidateSampleSceneSimplePasses()
+        {
+            // Use the Assert class to test conditions
+            var go = new GameObject("MyGameObject");
+            Assert.AreEqual("MyGameObject", go.name);
+        }
+
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator ValidateSampleSceneWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
